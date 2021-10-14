3207bd5fbd9181913434045ab8ac6b86520600c9
Umesh Krishnaswamy <krishji@gmail.com>
2021-10-14 13:19:12 +0530

    Update WANLP benchmark result plotting and notes

---
+++ LPBench/README.md
@@ -0,0 +1,53 @@
+# LP Solver Benchmark Results
+
+This page documents the benchmark results of the LP solvers. The solvers have
+been tested against the following benchmarks.
+
+- WANLPv2 is a benchmark of 29 models, 26 are performance-oriented and 3 are for
+  checking completion with default zero tolerance settings. WANLPv2 benchmark
+  models are in
+  [sharepoint](https://microsoft.sharepoint.com/:f:/t/AzNet_WAN/Eta127_8eHhNsylPOEupNQwB1OwjjRGGPilJtk2cf0sT_Q?e=cYFuIm)
+  and explanation of the models is in Section 6 of [this
+  document](https://microsoft.sharepoint.com/:b:/t/AzNet_WAN/Ed82YFQIC5xBg5_2ya0Y_bgB5OTmeu9GGMhEyFH6D7AXFg?e=GkPifF).
+
+
+## LPSharp cheatsheet
+
+LPSharp can be used to collect benchmark results. It can read MPS model files,
+set up the model in the solver, and execute the solver. The steps below read two
+models from uncompressed and compressed model files, and invokes CLP, GLOP, and
+MSF solver on a model.
+
+```
+LPSharp> read-mps model1.mps -key m1        # reads model 1 from an MPS file
+LPSharp> read-mps model2.mps.gz -key m2     # reads model 2 from a gzipped MPS file
+LPSharp> invoke-msfsolve -key m1            # invokes MSF solver on model 1
+LPSharp> invoke-clpsolve -key m1            # invokes CLP solver on model 1
+LPSharp> invoke-glopsolve -key m1           # invokes GLOP solver on model 1
+```
+
+## WANLPv2 results
+
+Please see results_wanlpv2.csv for the optimal values and solve times.
+Measurements were done on:
+
+- Neil's laptop Intel Core i7 7500U 2.7Ghz 4 logical processors 16GB.
+- Zainab's desktop Xeon 3+Ghz 64GB.
+
+You can plot the results by executing:
+
+```
+$ python plot_lpbench.py --help  # for usage
+$ python plot_lpbench.py results_wanlpv2.csv --baseline MSF_i7 --measurements CLPDual_i7 CLPPrimal_i7
+```
+
+CLP results are using Clp.exe built from CLP code in this repository using two
+solve methods: dual simplex and primal simplex. Reported times are iterations
+time + presolve time. MSF results are total times from Invoke-MSFSolve using
+Network Designer. Note that these results are not from LPSharp.
+
+CLP has a dual Simplex, primal Simplex, idiot solve methods. In terms of
+performance idiot is not fast, dual simplex is very fast but took a while with
+sonal-* mps models. Primal simplex solved sonal-* models quickly but not the
+edge and sliceperf ones (primal becomes infeasible and it needs to readjust and
+solve).
--- LPBench/plot_lpbench.py
+++ LPBench/plot_lpbench.py
@@ -1,57 +1,35 @@
+import argparse
+import csv
 import matplotlib.pyplot as plt
 import numpy as np
 import statistics
 
-# WANLPv2 benchmark results. Measurements were collected on Neil's laptop Intel
-# Core i7 7500U 2.7Ghz 4 logical processors 16GB.
-#
-# CLP results are using Clp.exe built from CLP code in this repository using two
-# solve methods: dual simplex and primal simplex. Reported times are iterations
-# time + presolve time. MSF results are total times from Invoke-MSFSolve using
-# Network Designer. Note that these results are not from LPSharp.
-#
-# Each data tuple is model name, CLP dualSimplex, primalSimplex, MSF primal.
-data = [
-('edge-pri0-diverse5.mps', 2382 + 420, 10492 + 440, 61608),
-('edge-pri0-maxmin0.mps', 4612 + 380, 3652 + 380, 27002),
-('edge-pri0-maxmin1.mps', 5382 + 430, 6192 + 430, 52156),
-('edge-pri0-maxmin2.mps', 6452 + 420, 8192 + 410, 54399),
-('edge-pri0-maxmin3.mps', 7782 + 410, 8262 + 400, 54138),
-('edge-pri0-maxmin4.mps', 6212 + 450, 7272 + 420, 53300),
-('edge-pri0-mincost.mps', 1752 + 400, 10592 + 390, 45836),
-('edge-pri0-minutil.mps', 2292 + 820, 4652 + 820, 41737),
-('sliceperf-pri0-diverse5.mps', 1842 + 510, 4722 + 50, 42852),
-('sliceperf-pri0-maxmin0.mps', 2782 + 440, 1462 + 470, 20690),
-('sliceperf-pri0-maxmin1.mps', 3262 + 470, 5562 + 490, 40737),
-('sliceperf-pri0-maxmin2.mps', 4172 + 460, 9812 + 470, 42158),
-('sliceperf-pri0-maxmin3.mps', 4392 + 480, 15682 + 480, 41377),
-('sliceperf-pri0-maxmin4.mps', 3692 + 470, 14672 + 470, 39991),
-('sliceperf-pri0-maxmin5.mps', 3982 + 460, 17072 + 470, 40162),
-('sliceperf-pri0-mincost.mps', 1632 + 440, 5012 + 440, 36582),
-('sliceperf-pri3-minutil.mps', 312 + 210, 322 + 210, 3252),
-('sonal-diverse1.mps', 542 + 240, 772 + 240, 9931),
-('sonal-diverse5.mps', 532 + 250, 742 + 240, 9630),
-('sonal-maxmin0.mps', 762 + 280, 602 + 260, 4699),
-('sonal-maxmin1.mps', 6572 + 280, 762 + 270, 9908),
-('sonal-maxmin2.mps', 15202 + 290, 902 + 280, 10190),
-('sonal-maxmin3.mps', 72272 + 280, 1492 + 280, 10047),
-('sonal-maxmin4.mps', 38832 + 280, 1222 + 270, 10262),
-('sonal-maxmin5.mps', 41112 + 280, 1362 + 280, 9966),
-('sonal-mincost.mps', 482 + 220, 732 + 230, 8908),
-('wander-minutil1.mps', 9482 + 60, 3642 + 60, 60000),
-('wander-primal1.mps', 20, 20, 11),
-('wander-primal2.mps', 20, 20, 3),
-]
 
-# Strip the wander results since they are for completion, not performance.
-data = data[:-3]
+def load_data(filename):
+    """Loads results from a CSV file."""
+    data = {}
+    with open(filename,'r') as csvfile:
+        reader = csv.DictReader(csvfile, lineterminator='\n')
+        for row in reader:
+            if 'Model' not in row or row['Model'].startswith('#'):
+                continue
+            for key, value in row.items():
+                key = key.strip()
+                if key != 'Model':
+                    value = float(value)
+                if key not in data:
+                    data[key] = []
+                data[key].append(value)
+        return data
 
-def process_results(data, baseidx, measureidx):
+
+def process_results(data, basekey, measurekey):
     """Returns data for a plot showing performance of measurement vs baseline."""
-    n = len(data)
-    baselines = np.array([data[i][baseidx] for i in range(n)])
-    measurements = np.array([data[i][measureidx] for i in range(n)])
-
+    baselines = np.array(data[basekey])
+    measurements = np.array(data[measurekey])
+    if len(baselines) != len(measurements):
+        raise UserWarning('Unequal length of {} and {} arrays, {} != {}'.format(basekey, measurekey, len(basekey), len(measurekey)))
+    n = len(baselines)
     speedups = np.zeros(n)
     for i in range(n):
         base = baselines[i]
@@ -62,26 +40,27 @@ def process_results(data, baseidx, measureidx):
         speedups[i] = ratio
 
     x = np.arange(n)
-    xlabels = np.array([data[i][0] for i in range(n)])
-    return x, xlabels, speedups
+    xlabels = np.array(data['Model'])
+    label = '{}/{}'.format(basekey, measurekey)
+    return x, xlabels, speedups, label
 
-def compute_means(data):
-    """Computes means of different combinations."""
-    m = len(data[0])
-    n = len(data)
-    x = []
-    y = []
 
-    for comparison in [(3, 1, 'MSF/CLPDual'), (3, 2, 'MSF/CLPPrimal')]:
-        idx1 = comparison[0]
-        idx2 = comparison[1]
-        x.append(comparison[2])
-        series1 = np.array([data[i][idx1] for i in range(n)])
-        series2 = np.array([data[i][idx2] for i in range(n)])
+def compute_means(data, basekey, measurekeys):
+    """Returns data for a plot showing harmonic means of measurements vs baseline."""
+    x, y = [], []
+
+    for key in measurekeys:
+        label = '{}/{}'.format(basekey, key)
+        x.append(label)
+
+        series1 = np.array(data[basekey])
+        series2 = np.array(data[key])
         ratios = series1 / series2
         mean = statistics.harmonic_mean(ratios)
         y.append(mean)
+
     return x, y
+
 
 def autolabel(rects, texts, offset, color):
     """Labels the bars with text."""
@@ -97,40 +76,73 @@ def autolabel(rects, texts, offset, color):
                     textcoords="offset points",
                     ha='center', va='bottom')
 
-# plt.rcParams['font.family'] = 'cmr10'
-plt.rcParams['font.size'] = fontsize = 11
-plt.rcParams['savefig.bbox'] = 'tight'
-plt.rcParams['savefig.pad_inches'] = 0.05
-plt.rcParams['savefig.dpi'] = 300
 
-fig, axes = plt.subplots(nrows=2, ncols=1)
-width = 0.8
+if __name__ == '__main__':
 
-ax = axes[0]
-x, xlabels, y = process_results(data, 3, 1)
-ax.plot(x, y, marker='s', ms=4, markevery=1, ls='-', label='MSF/CLPDual')
-_, _, y = process_results(data, 3, 2)
-ax.plot(x, y, marker='o', ms=4, markevery=1, ls='-', label='MSF/CLPPrimal')
-ax.set_xticks(x)
-ax.set_xticklabels(xlabels, rotation=75)
-ax.set_yticks([-10, -5, -1, 1, 5, 10, 15, 20])
-ax.axhline(1, color='tab:gray')
-ax.axhline(-1, color='tab:gray')
-ax.add_patch(plt.Rectangle((-1, -1), len(x)+1, 2, edgecolor='tab:gray', fill=False, hatch='///'))
-# ax.set_xlabel('Model')
-ax.set_ylabel('Speedup or slowdown')
-ax.annotate('Speedup', xy=(0,2.5), va='top')
-ax.annotate('Slowdown', xy=(0,-3), va='bottom')
-ax.legend()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('results_file', help='Results CSV file')
+    parser.add_argument('--baseline', '-b', type=str, help='Baseline column name')
+    parser.add_argument('--measurements', '-m', type=str, nargs='+', help='Measurement column names')
 
-ax = axes[1]
-x, y = compute_means(data)
-rects = ax.bar(x, y, width, fill=True, color='tab:cyan')
-autolabel(rects, np.around(y,1), 1, 'black')
-ax.axhline(1, color='tab:gray')
-ax.set_ylabel('Mean speedup')
+    args = parser.parse_args()
+    results_file = args.results_file
+    basekey = args.baseline
+    measurekeys = args.measurements
 
-fig.set_tight_layout(True)
-fig.set_size_inches(10.0, 8.0)
-# plt.savefig('results.pdf', format='pdf', bbox_inches='tight')
-plt.show()
+    # If no baseline is given, then make the first measurement index the baseline.
+    if basekey == None and measurekeys != None and len(measurekeys) > 0:
+        basekey = measurekeys[0]
+        measurekeys = measurekeys[1:]
+    if measurekeys == None or len(measurekeys) == 0:
+        raise UserWarning('No measurement columns to plot')
+
+    # Load the data file. Check that the baseline and measurement column names
+    # are present in the data.
+    data = load_data(results_file)
+    if len(data) == 0:
+        raise UserWarning('No data in {}'.format(results_file))
+    if basekey not in data:
+        raise UserWarning('Baseline column {} not found in results file {}'.format(basekey, results_file))
+    for key in measurekeys:
+        if key not in data:
+            raise UserWarning('Measurement column {} not found in results file {}'.format(key, results_file))
+
+    # plt.rcParams['font.family'] = 'cmr10'
+    plt.rcParams['font.size'] = fontsize = 11
+    plt.rcParams['savefig.bbox'] = 'tight'
+    plt.rcParams['savefig.pad_inches'] = 0.05
+    plt.rcParams['savefig.dpi'] = 300
+
+    fig, axes = plt.subplots(nrows=2, ncols=1)
+    width = 0.8
+
+    ax = axes[0]
+    markers = ['s', 'o', '^', 'v']
+    i = 0
+    for key in measurekeys:
+        x, xlabels, y, label = process_results(data, basekey, key)
+        marker = markers[i % len(markers)]
+        i += 1
+        ax.plot(x, y, marker=marker, ms=4, markevery=1, ls='-', label=label)
+    ax.set_xticks(x)
+    ax.set_xticklabels(xlabels, rotation=75)
+    ax.set_yticks([-10, -5, -1, 1, 5, 10, 15, 20])
+    ax.axhline(1, color='tab:gray')
+    ax.axhline(-1, color='tab:gray')
+    ax.add_patch(plt.Rectangle((-1, -1), len(x)+1, 2, edgecolor='tab:gray', fill=False, hatch='///'))
+    ax.set_ylabel('Speedup or slowdown')
+    ax.annotate('Speedup', xy=(0,2.5), va='top')
+    ax.annotate('Slowdown', xy=(0,-3), va='bottom')
+    ax.legend()
+
+    ax = axes[1]
+    x, y = compute_means(data, basekey, measurekeys)
+    rects = ax.bar(x, y, width, fill=True, color='tab:cyan')
+    autolabel(rects, np.around(y,1), 1, 'black')
+    ax.axhline(1, color='tab:gray')
+    ax.set_ylabel('Mean speedup')
+
+    fig.set_tight_layout(True)
+    fig.set_size_inches(10.0, 8.0)
+    # plt.savefig('lpbench.pdf', format='pdf', bbox_inches='tight')
+    plt.show()
