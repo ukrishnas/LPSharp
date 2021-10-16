be8dcb13a4ab1bfef853c89eacc91bd9262b68ba
Umesh Krishnaswamy <krishji@gmail.com>
2021-10-16 16:43:34 +0530

    Fix project version and add tweak to version to differentiate from public packages.

--- GLOP/cmake/utils.cmake
+++ GLOP/cmake/utils.cmake
@@ -4,29 +4,20 @@ function(get_patch_from_git VERSION_PATCH)
     message(STATUS "Setting patch to default. Did not find git package.")
     set(PATCH 9999)
   else()
-    # Using git describe, find human readable to tip of HEAD.
+    # Count the number of commits in this branch.
     execute_process(COMMAND
-      ${GIT_EXECUTABLE} describe --tags
+      ${GIT_EXECUTABLE} rev-list HEAD --count
       RESULT_VARIABLE _OUTPUT_VAR
       OUTPUT_VARIABLE PATCH
       ERROR_QUIET
       WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
-    if(NOT _OUTPUT_VAR)
-      # Simply count the number of commits in this branch.
-      execute_process(COMMAND
-        ${GIT_EXECUTABLE} rev-list HEAD --count
-        RESULT_VARIABLE _OUTPUT_VAR
-        OUTPUT_VARIABLE PATCH
-        ERROR_QUIET
-        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
-    endif()
-    if (PATCH)
-      # Trim spaces and new lines.
-      string(REGEX REPLACE " |\n" "" PATCH ${PATCH})
-    else()
-      message(STATUS "Setting patch to default. Current source directory may not be a git repository.")
-      set(PATCH 9999)
-    endif()
+  endif()
+
+  if (_OUTPUT_VAR)
+    message(STATUS "Setting patch to default. Current source directory may not be a git repository.")
+    set(PATCH 9999)
+  else()
+    string(REGEX REPLACE " |\n" "" PATCH ${PATCH})
   endif()
   set(${VERSION_PATCH} ${PATCH} PARENT_SCOPE)
 endfunction()
@@ -43,12 +34,14 @@ function(set_version VERSION)
     endif()
   endforeach()
 
-  # Compute patch if .git is present otherwise set it to 9999
-  get_filename_component(GIT_DIR ".git" ABSOLUTE)
-  if(EXISTS ${GIT_DIR})
+  # Set to false if git patch is not required.
+  if(TRUE)
     get_patch_from_git(PATCH)
   else()
     set(PATCH 9999)
   endif()
-  set(${VERSION} "${MAJOR}.${MINOR}.${PATCH}" PARENT_SCOPE)
+
+  # Force set tweak to differentiate from pubic versions.
+  set(TWEAK 1)
+  set(${VERSION} "${MAJOR}.${MINOR}.${PATCH}.${TWEAK}" PARENT_SCOPE)
 endfunction()
