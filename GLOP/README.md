b7441326ef4053a71fd31df9c50cd2be3376ec74
Umesh Krishnaswamy <krishji@gmail.com>
2021-10-21 16:35:20 +0530

    Add a custom rebase script for GLOP source code

--- GLOP/README.md
+++ GLOP/README.md
@@ -23,7 +23,39 @@ __What has been retained?__
 
 __What has been changed?__
 
-- CMake top-level file to alter defaults and remove unneeded functionality
+- Build system changes:
+  - The top-level CMakeLists.txt has been simplified and default values changed
+    for a Windows dotnet build.
+  - Only the needed cmake modules have been retained in the cmake/ folder and
+    they have been made simpler.
+  - Dependencies/CMakeLists.txt is used fetch the dependencies, abseil-cpp,
+    protobuf, swig, and zlib from their git repositories and patched using
+    patches in patches/.
+  - Deps.cmake checks that the dependencies are present, and fails the build if
+    they are not.
+  - Utils.cmake has two functions to generate the project version. We use a
+    4-number version, major.minor.patch.tweak, to differentiate our build from
+    the public nuget which does not use tweak. Major and minor numbers are from
+    Version.txt, patch is the count of commits in the branch, and tweak is
+    always 1.
+  - Cpp.cmake builds and installs the C++ sources. This in turn uses
+    CMakeLists.txt in each ortools/ subdirectory: base, glop, linear_solver,
+    lp_data, port, and util.
+  - Dotnet.cmake generates C# sources using Swig code in csharp/ folders in
+    linear_sharp and utils subdirectories. It generates dotnet build files using
+    input files in ortools/dotnet. In the build step, it produces two nuget
+    packages, Google.OrTools.version.nupkg and
+    Google.OrTools.runtime.win-x64.version.nupkg. Ortools/dotnet files.
+- The intent of the following changes is to fix code that references non-LP
+  modules, like graph and algorithms, that are not part of the tree. 
+  - Graph/iterators.h has been moved to util/ and the includes of this header
+    file changed to the new location. This was originally meant for graph
+    iteration but its usage has leaked into sparse_vector.h and a few other
+    files.
+  - Lp_data/lp_decomposer.{cc,h} files, which define the LPDecomposer type, were
+    deleted. LPDecomposer is only used by binary optimization integer solver
+    which has been removed. It references dynamic partitioning algorithm
+    which is not part of the tree.
 
 __What has been removed?__
 
@@ -33,7 +65,8 @@ __What has been removed?__
   - All sample code (samples/ folders)
 - Functionality:
   - Interfaces for third-party solvers (COIN, GLPK, Gurobi, SCIP, Xpress)
-  - Code for satisfiability and constrained programming solvers
+  - Boolean optimization solver
+  - Constraint programming solver
   - Code for combinatorial optimization and graph algorithms
   - Code for vehicle routing
 - Language support:
@@ -53,3 +86,79 @@ __What has been removed?__
   - Binary tools for Windows
   - Docker files
   - F# project files
+
+## Building the sources
+
+Please download [CMake](https://cmake.org). CMake has excellent
+[documentation](https://cmake.org/cmake/help/v3.22/). Try the
+[tutorial](https://cmake.org/cmake/help/v3.22/guide/tutorial/index.html) to get
+started with the tool. CMake is a two step build process. The first step
+generates the build system, which is Visual Studio project files in our case.
+The second step builds the targets using native build tools. You can also build
+using Visual Studio or msbuild, or browse code using the generated .sln file.
+
+The examples below assume that the current directory is the root of GLOP sources
+(which would likely be the same directory of this Readme), and your desired
+build directory is `./build`. 
+
+Generate the build system and build and fetch dependent packages. No options are
+required.
+
+```
+$ cmake -S . -B build
+```
+
+Build the release. The built packages will be in `build/dotnet/packages`.
+
+```
+$ cmake --build build --config Release
+```
+
+To directly import the package into your C# project, define a local source. This
+step is not required if you are going to publish the package to a remote nuget
+source.
+
+`nuget.config`:
+```
+<?xml version="1.0" encoding="utf-8"?>
+<configuration>
+    <packageSources>
+        <add key="WANLP" value="path-to-tree/build/dotnet/packages" />
+        <add key="nuget.org" value="https://api.nuget.org/v3/index.json" protocolVersion="3" />
+    </packageSources>
+</configuration>
+```
+
+Add the package to your C# project.
+
+```
+$ dotnet add package Google.OrTools --version <4-number-version>
+```
+
+## Merging new code
+
+Let's say you wish to merge new code from the public repository. The changes to
+source files are maintained as patch files in `patches/wanlp_<n>_<text>.patch`
+files. A helpful python script `patches/wanlp_rebase.py` applies the patches and
+other changes.
+
+I would not try to merge any of the CMake code. The build system in this
+repository has deviated significantly from the public repository. Also, our goal
+of dotnet Windows build is narrower than the goal of public maintainers. Even if
+source files are added or removed in listed ortools/ subdirectories, the CMake
+files do not need any changes.
+
+This is a snapshot of the execution of the rebase script. Let `private` be
+the current private tree, `public` be the public repository with the new
+code, and `ortools-copy` be the new copy. This script will copy the relevant
+files from the public and private trees, and apply the private patches.
+
+```
+$ python private\GLOP\patches\wanlp_rebase.py public private\GLOP ortools-copy --apply_patches
+Deleting ortools-copy. Press [yY] to continue: y
+Apply .\patches\wanlp-0-initial.patch, p to pick, c to check, any other letter to skip: p
+```
+
+
+
+
---
+++ GLOP/patches/wanlp_rebase.py
@@ -0,0 +1,199 @@
+import argparse
+from fnmatch import fnmatch
+import glob
+import logging
+import os
+import re
+import shutil
+import subprocess
+
+# Module logger
+_outputfolder = os.getcwd()
+_logger = logging.getLogger(__name__)
+_logger.setLevel(logging.DEBUG)
+_logfile = 'wanlp_rebase.log'
+
+def setup_logging(loglevel):
+    """Set up logger."""
+    numeric_level = getattr(logging, loglevel.upper(), None)
+    if not isinstance(numeric_level, int):
+        print('Invalid log level: {}'.format(loglevel), end=' ')
+        numeric_level = logging.WARNING
+        print('Using {}'.format(numeric_level))
+
+    global _logger
+    _logger = logging.getLogger(__name__)
+    _logger.setLevel(logging.DEBUG)
+    
+    # Create a handler at debug level that logs to file
+    handler1 = logging.FileHandler(os.path.join(_outputfolder, _logfile))
+    handler1.setLevel(logging.DEBUG)
+    handler1.setFormatter(logging.Formatter('%(asctime)s %(levelname)s %(message)s'))
+    _logger.addHandler(handler1)
+    
+    # Create a handler that logs to stderr
+    handler2 = logging.StreamHandler()
+    handler2.setLevel(numeric_level)
+    handler2.setFormatter(logging.Formatter('%(message)s'))
+    _logger.addHandler(handler2)
+
+def check_dir(dirname, expect):
+    '''Checks if a directory is present when it should be and raise an exception.'''
+    got = os.path.exists(dirname)
+    if got != bool(expect):
+        if expect:
+            _logger.error('Directory {} not present',format(dirname))
+            raise FileNotFoundError
+        else:
+            _logger.error('Directory {} already exists'.format(dirname))
+            raise FileExistsError
+
+def public_ortools_ignore(dirname, filenames):
+    '''Custom ignore function for copying files from the public ortools repository.'''
+    ignore = []
+    allow = []
+
+    for filename in filenames:
+
+        # Ignore third-party and non-LP solvers in ortools/linear_solver.
+        # And only keep the glop interface files.
+        if fnmatch(dirname, '*linear_solver'):
+            if fnmatch(filename, 'bop*') or \
+                fnmatch(filename, 'gurobi*') or \
+                fnmatch(filename, 'sat*') or \
+                fnmatch(filename, 'scip*'):
+                ignore.append(filename)
+                continue
+            reobj = re.compile('(?<!glop)_interface')
+            if reobj.search(filename) != None:
+                ignore.append(filename)
+                continue
+        
+        # Do not copy lp_data/lp_decomposer files.
+        if fnmatch(dirname, '*lp_data'):
+            if fnmatch(filename, 'lp_decomposer*'):
+                ignore.append(filename);
+                continue
+
+        # Allow the following source directories and files.
+        if fnmatch(filename, '*cc') or \
+            fnmatch(filename, '*.cs') or \
+            fnmatch(filename, '*.h') or \
+            fnmatch(filename, '*.i') or \
+            fnmatch(filename, '*.proto') or \
+            fnmatch(filename, 'base') or \
+            fnmatch(filename, 'csharp') or \
+            fnmatch(filename, 'dotnet') or \
+            fnmatch(filename, 'glop') or \
+            fnmatch(filename, 'linear_solver') or \
+            fnmatch(filename, 'linear_solver_natural_api.py') or \
+            fnmatch(filename, 'lp_data') or \
+            fnmatch(filename, 'ortools') or \
+            fnmatch(filename, 'port') or \
+            fnmatch(filename, 'util'):
+            allow.append(filename)
+            continue
+
+        # Allow these miscellaneous files.
+        if fnmatch(filename, 'LICENSE*.txt') or \
+            fnmatch(filename, 'Version.txt') or \
+            (fnmatch(dirname, 'patches') and fnmatch(filename, '*patch')):
+            allow.append(filename)
+            continue
+
+        # Ignore the rest.
+        ignore.append(filename)
+    _logger.info('public_ortools_ignore dirname={} allow={} ignore={}'.format(dirname, allow, ignore))
+    return ignore
+
+
+def private_ortools_ignore(dirname, filenames):
+    '''Custom ignore function for copying files from the private ortools repository.'''
+    ignore = []
+    allow = []
+
+    for filename in filenames:
+        # Always exclude the build directory.
+        if fnmatch(dirname, 'build*'):
+            ignore.append(filename)
+            continue
+
+        # Allow CMake build files.
+        if fnmatch(filename, 'cmake') or \
+            fnmatch(dirname, '*cmake') or \
+            fnmatch(filename, '*CMakeLists.txt*') or \
+            fnmatch(filename, 'dotnet') or \
+            fnmatch(dirname, '*dotnet*') or \
+            fnmatch(filename, 'ortools') or \
+            fnmatch(dirname, '*ortools'):
+            allow.append(filename)
+            continue
+
+        # Allow these miscellaneous files.
+        if fnmatch(filename, 'README.md') or \
+            fnmatch(filename, 'patches') or \
+            fnmatch(dirname, '*patches'):
+            allow.append(filename)
+            continue
+
+        # Ignore the rest.
+        ignore.append(filename)
+    _logger.info('private_ortools_ignore dirname={} allow={} ignore={}'.format(dirname, allow, ignore))
+    return ignore
+
+def apply_private_patches(dirname):
+    '''Apply private patches.
+    TODO(user): redirect standard output in subprocess call.
+    '''
+
+    os.chdir(dirname)
+
+    globpath = os.path.join('.', 'patches', 'wanlp*.patch')
+    patch_files = glob.glob(globpath)
+    if patch_files == None or len(patch_files) == 0:
+        _logger.warning('No patches found matching {}'.format(globpath))
+
+    for patch_file in patch_files:
+        response = input('Apply {}, p to pick, c to check, any other letter to skip: '.format(patch_file))
+        response = response.lower()
+        if response.startswith('p'): 
+            _logger.info('Applying patch file {}'.format(patch_file))
+            subprocess.call(['git', 'apply', patch_file])
+        elif response.startswith('c'):
+            _logger.info('Checking patch file {}'.format(patch_file))
+            subprocess.call(['git', 'apply', '--check', patch_file])
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('public_tree', help='The path to the new public tree')
+    parser.add_argument('private_tree', help='The path to the current private tree')
+    parser.add_argument('copy_dst', help='The path to the new copy destination')
+    parser.add_argument('--apply_patches', action='store_true', default=False, help='Apply private patches to new copy')
+    parser.add_argument('--loglevel', default='WARN', help='Logging level')
+
+    args = parser.parse_args()
+    setup_logging(args.loglevel)
+    _logger.info('Starting new copy args={}'.format(args))
+
+    public_tree = args.public_tree
+    private_tree = args.private_tree
+    copy_dst = args.copy_dst
+    check_dir(public_tree, expect=True)
+    check_dir(private_tree, expect=True)
+
+    if os.path.exists(copy_dst):
+        _logger.info('Deleting copy destination tree {}'.format(copy_dst))
+        response = input('Deleting {}. Press [yY] to continue: '.format(copy_dst))
+        if response.lower().startswith('y'):
+            shutil.rmtree(copy_dst)
+
+    _logger.info('Copying private tree files to destination, private={} destination={}'.format(private_tree, copy_dst))
+    shutil.copytree(private_tree, copy_dst, ignore=private_ortools_ignore, dirs_exist_ok=True)
+    
+    _logger.info('Copying public tree files to destination, public={} destination={}'.format(public_tree, copy_dst))
+    shutil.copytree(public_tree, copy_dst, ignore=public_ortools_ignore, dirs_exist_ok=True)
+
+    if args.apply_patches:
+        _logger.info('Applying private patches')
+        apply_private_patches(copy_dst)
